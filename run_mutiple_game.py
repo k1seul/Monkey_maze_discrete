0e7865a431b78eb5a2eeb20c31f5448b516aabb8
k1seul <briankan21@gmail.com>
2023-05-14 13:04:41 +0900

    running changes

--- run_mutiple_game.py
+++ run_mutiple_game.py
@@ -1,6 +1,7 @@
 from agent_train import agent_train
 
-game_ver = 3 
-agent_train(uniform_sample=True, TD_sample=False, game_version=game_ver, agent_memory_based=False)
+game_ver = 0
 agent_train(uniform_sample=True, TD_sample=False, game_version=game_ver, agent_memory_based=True)
+agent_train(uniform_sample=True, TD_sample=False, game_version=game_ver, agent_memory_based=False)
+agent_train(uniform_sample=True, TD_sample=True, game_version=game_ver, agent_memory_based=True)
 
