37c686fa9022a73ee6981e000b4ce6b370920f35
k1seul <briankan21@gmail.com>
2023-05-31 15:59:16 +0900

    fixed length

--- run_tensorboard.py
+++ run_tensorboard.py
@@ -5,5 +5,5 @@ import subprocess
 log_dir = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/tensorboard_Data')
 
 
-port = 6111 ## random.randint(6000, 7000)
+port = 6321 ## random.randint(6000, 7000)
 subprocess.Popen(f"tensorboard --logdir={log_dir} --port={port} --reload_multifile=true", shell=True)
